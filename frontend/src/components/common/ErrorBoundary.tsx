8c1b684733d6ab25e833e1442a45a9bc194715e4
OtakuForLife <lukas.kuehl@protonmail.com>
2025-06-13 17:15:31 +0200

    remove unused react import

--- frontend/src/components/common/ErrorBoundary.tsx
+++ frontend/src/components/common/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ErrorInfo, ReactNode } from 'react';
+import { Component, ErrorInfo, ReactNode } from 'react';
 import { log } from '@/lib/logger';
 import { ERROR_MESSAGES } from '@/config/constants';
 
