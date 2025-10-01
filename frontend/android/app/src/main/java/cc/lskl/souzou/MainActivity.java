749195fc769e257b85fc3de6e3e03a8665c87f9e
OtakuForLife <lukas.kuehl@protonmail.com>
2025-10-01 08:24:38 +0200

    init capactor and add android project

---
+++ frontend/android/app/src/main/java/cc/lskl/souzou/MainActivity.java
@@ -0,0 +1,5 @@
+package cc.lskl.souzou;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cc.lskl.souzou',
+  appName: 'souzou',
+  webDir: 'dist'
+};
+
+export default config;
